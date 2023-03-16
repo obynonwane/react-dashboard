73da826c4fe563de2daff2c3383bf881c04d3f32
obinna <biostechnologyng@gmail.com>
2023-03-16 10:52:08 +0100

    update

--- src/components/shared/Sidebar.jsx
+++ src/components/shared/Sidebar.jsx
@@ -1,10 +1,14 @@
 import React from "react";
+import classNames from "classnames";
 import { FcBullish } from "react-icons/fc";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import {
   DASHBOARD_SIDEBAR_BOTTOM_LINKS,
   DASHBOARD_SIDEBAR_LINKS,
 } from "../../lib/const/navigation";
+
+const linkClass =
+  "flex items-center gap-2 font-light px-3 py-2 hover:bg-neutral-700 hover:no-underline active:bg-neutral-600 rounded-sm text-base";
 
 export default function Sidebar() {
   return (
@@ -13,7 +17,7 @@ export default function Sidebar() {
         <FcBullish />
         <span className="text-neutral-100 text-lg">Openshop</span>
       </div>
-      <div className="flex-1">
+      <div className="flex-1 py-8 flex flex-col gap-0.5">
         {DASHBOARD_SIDEBAR_LINKS.map((item) => (
           <SidebarLink key={item.key} item={item} />
         ))}
@@ -24,8 +28,16 @@ export default function Sidebar() {
 }
 
 function SidebarLink({ item }) {
+  const { pathname } = useLocation();
   return (
-    <Link>
+    <Link
+      to={item.path}
+      className={classNames(
+        pathname === item.path ? "text-white" : "",
+        "text-white",
+        linkClass
+      )}
+    >
       <span className="text-xl">{item.icon}</span>
       {item.label}
     </Link>
